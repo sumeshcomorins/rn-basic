481f0da9a4e1ce6b9cd172fc8cc32bfc1e16613d
sumecomorins <sumesh@mail.comorins.com>
2022-07-21 23:52:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
